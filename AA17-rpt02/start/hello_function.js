c7f5c128cd0fd7eb38509e65eb10fb222f553c80
HINEET <79964312+HINEET@users.noreply.github.com>
2021-09-08 17:20:30 +0900

    Add files via upload

---
+++ AA17-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what) {
+    console.log("Hello "+what+" !");
+}
+
+hello("aa17");
+hello("HINEET, 장태호");
---
+++ AA17-rpt02/start/index_aa17.js
@@ -0,0 +1,5 @@
+var ohhi = require('./aa17info.js');
+
+ohhi("AA00", "Redwoods", "010-4561-1968");
+
+ohhi("AA17", "HINEET", "010-1234-5678");
